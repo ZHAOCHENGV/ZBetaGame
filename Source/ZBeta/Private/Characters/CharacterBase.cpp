e203b7a23dbec11e500f6a05ba9cc0ba107fcb9f
V <zcw1419@gmail.com>
2025-11-12 00:32:16 +0800

    删除多余文件

---
+++ Source/ZBeta/Private/Characters/CharacterBase.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/Characters/CharacterBase.h"
+
+
+
+ACharacterBase::ACharacterBase()
+{
+	
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void ACharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
